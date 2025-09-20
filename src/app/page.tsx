2d0f47cc9b620aabf5a6f6c91236dcd7e76b29c2
Christopher Bicknell <chrisb2812@gmail.com>
2025-09-20 15:49:20 +0100

    rechange of images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,8 +57,15 @@ export default function Home() {
                       alt="Elegant setting"
                       data-ai-hint="elegant interior"
                       fill
-                      className="object-cover"
+                      className="object-cover hidden md:block"
                   />
+                  <Image
+                    src="/small-p1-img2.webp"
+                    alt="Elegant setting"
+                    data-ai-hint="elegant interior"
+                    fill
+                    className="object-cover block md:hidden"
+                />
               </div>
             </div>
           </div>
@@ -70,15 +77,9 @@ export default function Home() {
                     alt="Elegant setting"
                     data-ai-hint="elegant interior"
                     fill
-                    className="object-cover hidden md:block"
+                    className="object-cover"
                 />
-                <Image
-                    src="/small-p1-img2.webp"
-                    alt="Elegant setting"
-                    data-ai-hint="elegant interior"
-                    fill
-                    className="object-cover block md:hidden"
-                />
+                
             <div className="absolute inset-0 bg-gradient-to-t from-transparent via-background/50 to-transparent" />   
             
           <div className="relative mx-auto px-4 text-center md:px-6">
