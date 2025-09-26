812fde094bb793602066f41ce00fd4ac450270a7
Christopher Bicknell <chrisb2812@gmail.com>
2025-09-26 13:28:56 +0100

    added adult meta to website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400;0,700;1,400&family=Great+Vibes&display=swap" rel="stylesheet" />
+        <meta name="rating" content="adult"></meta>
       </head>
       <body className="font-body antialiased">
         <div className="flex min-h-screen flex-col">
